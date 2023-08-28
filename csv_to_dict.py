744326ddca1af278c824fae242044d942af3e92c
Keep-On-Going <58552647+Keep-On-Going@users.noreply.github.com>
2023-08-28 23:57:28 +0800

    error with the lists connected to list
    something wrong with where the information is being added to also the \n is not being deleted

--- csv_to_dict.py
+++ csv_to_dict.py
@@ -4,10 +4,10 @@ Data_on_organisations = open("Organisations.csv","r") # this creates a file obje
 # note the file is meant to take an output from the 
 
 first_line_of_file = Data_on_organisations.readline() # this reads the topline of the file and returns it as a single string, readline adds a \n we need to get rid of this
-#first_line_of_file.rstrip("\n")
+first_line_of_file.rstrip("\n")
 #print(first_line_of_file.replace("\n",""))
 first_line_of_file = first_line_of_file.replace("\n","") # this gets rid of the \n from the first line
-list_of_headings = first_line_of_file.split(",") # this turns the string from the topline of the file into a list with each item being the respective headers
+list_of_headings = first_line_of_file.split(",") # this turns the string from the topline of the file into a list with each item being the respective headers, .split() outputs a copy of the list 
 #print(list_of_headings)
 
 dict_of_org_data ={} # empty dictionary where all the org data will be sorted into 
@@ -16,4 +16,16 @@ for i in list_of_headings: # this for loop adds dictionary items with the key be
     dict_of_org_data[i] = []
 
 
+list_of_org_dict_keys = list(dict_of_org_data)
+print(list_of_org_dict_keys[0])
+
+for line in Data_on_organisations:# this for loop loops through the remaining lines in csv file and sorts the data into the dictionary 
+    line.rstrip("\n")
+    current_line_list = line.split(",")
+    a = 0     # iterator 
+    for item in current_line_list:
+        dict_of_org_data[list_of_org_dict_keys[a]].append(item)
+    a += 1
+
+print(dict_of_org_data[list_of_org_dict_keys[0]])    
 
