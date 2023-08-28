3a55f73a3d43bf1e8dd28e98d91df468d1d9fcd2
Keep-On-Going <58552647+Keep-On-Going@users.noreply.github.com>
2023-08-28 13:45:12 +0800

    Update CITS1401.py

--- CITS1401.py
+++ CITS1401.py
@@ -1,2 +1,7 @@
 # python project for CITS1401
 
+# we need to find a way to manage the csv file data so we can edit it and access it easier and in its orders 
+# so we using a dictionary 
+
+# where is the csv file going to be stored ??????
+print(open("Organisations.csv","r"))
