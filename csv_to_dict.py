a238cdf47bb0875f62fb7e7faee51aac8a599797
Keep-On-Going <58552647+Keep-On-Going@users.noreply.github.com>
2023-08-29 01:05:44 +0800

    csv to dictionary works
    all the data is now going to the correct lists. It is now properly sorted.
    
    The #tester# segments should be deleted before submission. They help in checking.

--- csv_to_dict.py
+++ csv_to_dict.py
@@ -5,10 +5,10 @@ Data_on_organisations = open("Organisations.csv","r") # this creates a file obje
 
 first_line_of_file = Data_on_organisations.readline() # this reads the topline of the file and returns it as a single string, readline adds a \n we need to get rid of this
 first_line_of_file.rstrip("\n")
-#print(first_line_of_file.replace("\n",""))
+#tester#print(first_line_of_file.replace("\n",""))
 first_line_of_file = first_line_of_file.replace("\n","") # this gets rid of the \n from the first line, the .replace will output a copy of the string so needs to be set to a variable to be used 
 list_of_headings = first_line_of_file.split(",") # this turns the string from the topline of the file into a list with each item being the respective headers, .split() outputs a copy of the list 
-#print(list_of_headings)
+#tester#print(list_of_headings)
 
 dict_of_org_data ={} # empty dictionary where all the org data will be sorted into 
 i = 0
@@ -17,18 +17,21 @@ for i in list_of_headings: # this for loop adds dictionary items with the key be
 
 
 list_of_org_dict_keys = list(dict_of_org_data)
-print(list_of_org_dict_keys[0])
+#tester#print(list_of_org_dict_keys[0])
 
-#for line in Data_on_organisations:# this for loop loops through the remaining lines in csv file and sorts the data into the dictionary 
-    #line = line.replace("\n","") # gets rid of the "\n"
-#print(line)
-line = Data_on_organisations.readline()
-current_line_list = line.split(",")
-a = 0     # iterator 
-for item in current_line_list:
-    dict_of_org_data[list_of_org_dict_keys[a]].append(item)
-    print(dict_of_org_data[list_of_org_dict_keys[a]])
-    a += 1
+for line in Data_on_organisations:# this for loop loops through the remaining lines in csv file and sorts the data into the dictionary 
+    line = line.replace("\n","") # gets rid of the "\n", the replace function needs to be set to an variable as it outputs a copy of the list
+    #tester#print(line)
+    current_line_list = line.split(",") # this turns the current line's string into a list, with each item corresponding to a key in the dictionary 
+    a = 0     # iterator 
+    for item in current_line_list:
+        dict_of_org_data[list_of_org_dict_keys[a]].append(item)
+        #print(dict_of_org_data[list_of_org_dict_keys[a]])
+        a += 1
 
-#print(dict_of_org_data[list_of_org_dict_keys[0]])    
-
+#tester#print(dict_of_org_data[list_of_org_dict_keys[0]])    # this returns the list set to the first key
+print(dict_of_org_data[list_of_org_dict_keys[1]]) 
+print(dict_of_org_data[list_of_org_dict_keys[2]]) 
+print(dict_of_org_data[list_of_org_dict_keys[3]]) 
+print(dict_of_org_data[list_of_org_dict_keys[4]]) 
+print(dict_of_org_data[list_of_org_dict_keys[5]]) 
