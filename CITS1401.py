c4e2c678a619471cea020f735e16c1e6cd9ba987
Keep-On-Going <58552647+Keep-On-Going@users.noreply.github.com>
2023-09-06 19:55:58 +0800

    Update CITS1401.py

--- CITS1401.py
+++ CITS1401.py
@@ -99,24 +99,20 @@ def rangecheck(lowrange,highrange,pos_list,pos_header,csv_dict): # checks poslit
 
 # returns list with max and min company positions
 def high_and_low_count(csv_dict,poslist,pos_header): #poslist is the list of positions
-    start_number = int(csv_dict[0][csv_dict[1][pos_header]][0])
-    print(type(start_number))
+    start_number = int(csv_dict[0][csv_dict[1][pos_header]][poslist[0]])
     largest_number = start_number
     smallest_number = start_number
     max_company_list_pos = poslist[0]
     min_company_list_pos = poslist[0] 
-
-    print()
-
     for i in poslist:
         print(csv_dict[0][csv_dict[1][pos_header]][i])
         if int(csv_dict[0][csv_dict[1][pos_header]][i]) > largest_number:
             largest_number =  int(csv_dict[0][csv_dict[1][pos_header]][i])
             max_company_list_pos = i 
-        if  smallest_number > int(csv_dict[0][csv_dict[1][pos_header]][i]): #min not correct right now 
+        if  int(csv_dict[0][csv_dict[1][pos_header]][i]) < smallest_number : #min not correct right now 
             smallest_number = int(csv_dict[0][csv_dict[1][pos_header]][i])
             min_company_list_pos = i
-        print(min_company_list_pos)
+        
     return [max_company_list_pos,min_company_list_pos]      
 
 def main(csvfile,country):
